b2ef5c7a2534e831e46a10a4292cc430806f58cc
Yashjain1623 <157748380+Yashjain1623@users.noreply.github.com>
2024-07-29 15:11:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
 
-function SportsCar(make, model, topSpeed) {}
+// Add a method to the Car prototype
+Car.prototype.getMakeModel = function() {
+  return `${this.make} ${this.model}`;
+};
+
+function SportsCar(make, model, topSpeed) {
+  // Call the parent constructor
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
+}
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+// Add a method to the SportsCar prototype
+SportsCar.prototype.getTopSpeed = function() {
+  return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
